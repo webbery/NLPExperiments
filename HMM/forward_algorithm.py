eed647930c434520687dbbea5ce68b7f59aeb630
webberg <webbery@foxmail.com>
2019-08-06 17:45:25 +0800

    隐马尔科夫模型:forward_algorithm

---
+++ HMM/forward_algorithm.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+''' 
+O: observered event probablitics
+H: hide params to hide params probablitics
+oberserved: a serials of event
+start: 
+'''
+def forward_algorithm(O,H,oberserved,start):
+    prev_probs=defaultdict(dict)
+    def forward_impl(t,state):
+        if prev_probs.__contains__(t):
+            if prev_probs[t].__contains__(state):
+                return prev_probs[t][state]
+        if t==0:
+            # print(start[t][state])
+            return start[0][state]*O[state][oberserved[t]]
+        prob = sum([ H[prev_state][state]*O[state][oberserved[t]]*forward_impl(t-1,prev_state) for prev_state in H])
+        prev_probs[t][oberserved[t]] = prob
+        return prob
+
+    for idx in range(len(oberserved)):
+        for state in H.keys():
+            print(state,'->',oberserved[idx],':',forward_impl(idx,state))
---
+++ HMM/test.py
@@ -0,0 +1,17 @@
+import forward_algorithm
+
+observed_prob={
+    'hot':{'1':0.2,'2':0.4,'3':0.4},
+    'cold':{'1':0.5,'2':0.4,'3':0.1}
+}
+
+start_prob = {0:{'hot':0.8,'cold':0.2}}
+hide_prob={
+    'hot':{'hot':0.6,'cold':0.3,'end':0.1},
+    'cold':{'hot':0.4,'cold':0.5,'end':0.1}
+}
+
+observed1=['3','1','3']
+observed2=['3','3','1','1','2','2','3','1','3']
+
+forward_algorithm.forward_algorithm(observed_prob,hide_prob,observed2,start=start_prob)
